778cf82298288fdf29c8d00de9d27d2a74ea9f71
Altieres <altieres.silva@hotmail.com>
2024-11-17 02:10:11 -0300

    fix classe Bootcamp

--- src/main/java/br/altieres/challenger/domain/Bootcamp.java
+++ src/main/java/br/altieres/challenger/domain/Bootcamp.java
@@ -3,6 +3,7 @@ package br.altieres.challenger.domain;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class Bootcamp {
@@ -29,4 +30,38 @@ public class Bootcamp {
         this.descricao = descricao;
     }
 
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+
+    }
+
+    public Set<Dev> getDevsIncritos() {
+        return devsIncritos;
+    }
+
+    public void setDevsIncritos(Set<Dev> devsIncritos) {
+        this.devsIncritos = devsIncritos;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bootcamp bootcamp = (Bootcamp) o;
+        return Objects.equals(nome, bootcamp.nome) && Objects.equals(descricao, bootcamp.descricao) && Objects.equals(dataInicial, bootcamp.dataInicial) && Objects.equals(dataFinal, bootcamp.dataFinal) && Objects.equals(devsIncritos, bootcamp.devsIncritos) && Objects.equals(conteudos, bootcamp.conteudos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, descricao, dataInicial, dataFinal, devsIncritos, conteudos);
+
+    }
 }
